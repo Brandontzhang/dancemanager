d23d2fe3b91f554e7c181b446c605b411a490b87
Brandontzhang <zhangtbrandon2@gmail.com>
2023-08-22 22:48:05 -0400

    Fixed heat/final tracking and added comments

--- src/schema.ts
+++ src/schema.ts
@@ -13,6 +13,7 @@ export const typeDefs = gql`
         createAdmin(username: String, password: String, email: String): createAdminResponse!
     }
 
+    "Response containing details from a request creating a new admin account"
     type createAdminResponse {
         "Similar to HTTP status code, represents the status of the mutation"
         code: String!
@@ -24,6 +25,7 @@ export const typeDefs = gql`
         admin: Admin
     }
 
+    "Administrator account for running a competition"
     type Admin {
         "Primary key id"
         id: ID!
@@ -33,6 +35,7 @@ export const typeDefs = gql`
         email: String!
     }
 
+    "A competition holding references to judges, contestants, and events"
     type Competition {
         "Primary key id"
         id: ID!,
@@ -50,6 +53,7 @@ export const typeDefs = gql`
         eventQueue: [Event]!
     }
 
+    "A contestant is a couple with a number assigned"
     type Contestant {
         "Primary key id"
         id: ID!
@@ -58,7 +62,28 @@ export const typeDefs = gql`
         "Competition this registration is part of"
         competition: Competition
     }
-    
+
+    "Record tracking how a contestant performed in a particular round"
+    type ContestantRound {
+        "Primary key id"
+        id: ID!
+        "Reference to the contestant record"
+        contestant: Contestant!
+        "Tracking which judges marked the couple for this round"
+        judgeMarks: [Judge]
+    }
+
+    "Record tracking how a contestant perfomed in the final round, key difference from normal rounds is that actual placements are considered"
+    type ContestantFinal {
+        "Primary key id"
+        id: ID!
+        "Reference to the contestant record"
+        contestant: Contestant!
+        "Tracking which judges marked the couple for which place"
+        judgeMarks: [[Judge]]
+    }
+
+    "An event is made of several rounds, each comprising of at least one heat, culminating in a final with placements"
     type Event {
         "Primary key id"
         id: ID!
@@ -74,35 +99,43 @@ export const typeDefs = gql`
         final: Final
     }
 
+    "The final round of an event where contestants are ranked by the judges"
     type Final {
         "Primary key id"
         id: ID!
-        "Contestants participating in the final"
-        contestant: Contestant
-        "Array tracking which judges marked which palce"
-        judgeMarks: [[Judge]]
+        "Records for dontestants participating in the final"
+        contestantFinal: [ContestantFinal!]!
     }
 
+    "When a round has too many contestants to be ran all at once, it is broken down into heats"
     type Heat {
         "Primary key id"
         id: ID!
-        contestant: Contestant
-        judgeMarks: [Judge]
+        "Records for dontestants participating in the heat"
+        contestantMarks: [ContestantMarks!]!
     }
 
+    "Judge account marking contestants"
     type Judge {
         "Primary key id"
         id: ID!
+        "Name of the judge"
         name: String!
+        "Judge number for reference in case of anonimity"
         number: Int
     }
 
+    "One round of an event, comprising of at least one heat"
     type Round {
         "Primary key id"
         id: ID!
+        "Contestants participating in the round"
         contestants: [Contestant]!
+        "NUmber of heats in the round"
         heats: [Heat!]!
+        "Amount each judge is expected to recall for the next round (number they should mark down)"
         cutoff: Int
+        "Judges participating in the round"
         judges: [Judge]
     }
 `;
